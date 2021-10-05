9f83dbc128abba07c2e39b5bb97efd808370bb9a
HASH-002 <koserhasan002@gmail.com>
2021-10-05 14:40:42 +0530

    Adding input validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
 // Create a user using: POST "/api/auth/". Doesn't require Auth
-router.post('/',(req, res)=>{
-    console.log(req.body)
-    const user = User(req.body);
-    user.save();
-    res.send('Hello World!')
-})
+router.post(
+    "/",
+    [
+        body("name", "Enter a valid name").isLength({ min: 3 }),
+        body("email", "Enter a valid email").isEmail(),
+        body("password", "Password must be atleast 5 characters long").isLength({ min: 5 }),
+    ],
+    (req, res) => {
+        // Finds the validation errors in this request and wraps them in an object with handy functions
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        }).then((user) => res.json(user))
+            .catch(err => {
+                res.json({
+                    error: "Please enter a unique value for email",
+                    message: err.message
+                })
+            })
+    }
+);
 
 module.exports = router;
