c1445b3234f49a58ebd5cb20f7f4bf2bd4efda1d
HASH-002 <koserhasan002@gmail.com>
2021-10-06 15:43:27 +0530

    Added CRUD operation for Notes

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -1,22 +1,26 @@
 const mongoose = require('mongoose');
 var Schema = mongoose.Schema
 const NotesSchema = new Schema({
-    title:  {
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    title: {
         type: String,
         required: true
     },
-    description:  {
+    description: {
         type: String,
         required: true
     },
-    tag:  {
+    tag: {
         type: String,
         default: "General"
     },
-    date:  {
+    date: {
         type: Date,
-        default: Date.now 
+        default: Date.now
     }
-  });
+});
 
-module.exports = mongoose.model('notes',NotesSchema);
+module.exports = mongoose.model('notes', NotesSchema);
